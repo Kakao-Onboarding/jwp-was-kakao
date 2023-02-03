578cf4e3eef3c45afdbcdcf100470c6aae4866e1
Kloong1 <dkdlsmsm98@gmail.com>
2023-02-03 16:13:44 +0900

    feat: 정적 리소스 핸들러 구현

---
+++ src/main/java/webserver/handler/StaticResourceRequestHandler.java
@@ -0,0 +1,48 @@
+package webserver.handler;
+
+import utils.FileIoUtils;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
+import webserver.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StaticResourceRequestHandler {
+
+    public HttpResponse handle(HttpRequest httpRequest) {
+        byte[] bytes;
+        try {
+            bytes = FileIoUtils.loadFileFromClasspath("/static" + httpRequest.getURL());
+        } catch (Exception e) {
+            return  HttpResponse.HttpResponseBuilder.aHttpResponse()
+                    .withStatus(HttpStatus.BAD_REQUEST)
+                    .withVersion("HTTP/1.1")
+                    .build();
+        }
+
+        return HttpResponse.HttpResponseBuilder.aHttpResponse()
+                .withStatus(HttpStatus.OK)
+                .withVersion("HTTP/1.1")
+                .withHeaders(generateHeaders(httpRequest, bytes))
+                .withBody(bytes)
+                .build();
+    }
+
+    private Map<String, String> generateHeaders(HttpRequest httpRequest, byte[] body) {
+        Map<String, String> headers = new HashMap<>();
+
+        String url = httpRequest.getURL();
+        if (url.endsWith(".js")) {
+            headers.put("Content-Type", "text/javascript; charset=utf-8");
+        } else if (url.endsWith(".css")) {
+            headers.put("Content-Type", "text/css; charset=utf-8");
+        } else {
+            headers.put("Content-Type", "text/html; charset=utf-8");
+        }
+
+        headers.put("Content-Length", String.valueOf(body.length));
+
+        return headers;
+    }
+}
