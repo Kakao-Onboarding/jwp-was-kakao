cea1fd9b84ccccb1cae5bc5b4f1785dc97866ec5
Kloong1 <dkdlsmsm98@gmail.com>
2023-02-06 14:03:54 +0900

    feat: RequestHandler에 Handler 등록후 호출하는 로직 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,52 +2,99 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.IOUtils;
+import webserver.handler.Handler;
+import webserver.http.HttpRequest;
+import webserver.http.HttpRequestParser;
+import webserver.http.HttpResponse;
+import webserver.http.HttpStatus;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.*;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private final Map<String, Handler> urlHandlerMapping;
+    private final List<Handler> defaultHandlerMapping;
 
-    public RequestHandler(Socket connectionSocket) {
+    public RequestHandler(Socket connectionSocket, Map<String, Handler> urlHandlerMapping, List<Handler> defaultHandlerMapping) {
         this.connection = connectionSocket;
+        this.urlHandlerMapping = urlHandlerMapping;
+        this.defaultHandlerMapping = defaultHandlerMapping;
     }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            StringBuilder requestStringBuilder = new StringBuilder();
+
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+
+            requestStringBuilder.append(line).append("\r\n");
+
+            Map<String, List<String>> headers = new LinkedHashMap<>();
+            while ((line = br.readLine()) != null && !"".equals(line)) {
+                String[] headerAndVaules = line.split(": ");
+                String header = headerAndVaules[0];
+                List<String> values = new ArrayList<>();
+                Arrays.stream(headerAndVaules).skip(1).forEach(values::add);
+                headers.put(header, values);
+                requestStringBuilder.append(line).append("\r\n");
+            }
+
+            if (requestStringBuilder.length() == 0) {
+                System.out.println("exit");
+                return;
+            }
+
+            requestStringBuilder.append("\r\n");
+
+            if (headers.containsKey("Content-Length")) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length").get(0)));
+                requestStringBuilder.append(body);
+            }
+
+            System.out.println(requestStringBuilder.toString());
+
+            HttpRequest request = new HttpRequestParser().parse(requestStringBuilder.toString());
+
+            HttpResponse httpResponse = null;
+            Handler urlHandler = urlHandlerMapping.get(request.getURL());
+            if (urlHandler != null) {
+                httpResponse = urlHandler.handle(request);
+            } else {
+                for (Handler handler : defaultHandlerMapping) {
+                    try {
+                        httpResponse = handler.handle(request);
+                        break;
+                    } catch (Exception ignored) {}
+                }
+                if (httpResponse == null) {
+                    httpResponse = HttpResponse.HttpResponseBuilder.aHttpResponse()
+                            .withStatus(HttpStatus.BAD_REQUEST)
+                            .withVersion("HTTP/1.1")
+                            .build();
+                }
+            }
+
+            System.out.println(httpResponse);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello world".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
+            dos.writeBytes(httpResponse.toString());
+            if(httpResponse.getBody() != null){
+                dos.write(httpResponse.getBody());
+            }
             dos.flush();
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -3,32 +3,21 @@ package webserver;
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
 import utils.FileIoUtils;
+import webserver.handler.Handler;
+import webserver.handler.HtmlRequestHandler;
+import webserver.handler.StaticResourceRequestHandler;
+import webserver.handler.UserApiHandler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestHandlerTest {
-    @Test
-    void socket_out() {
-        // given
-        final var socket = new StubSocket();
-        final var handler = new RequestHandler(socket);
-
-        // when
-        handler.run();
-
-        // then
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 11 ",
-                "",
-                "Hello world");
-
-        assertThat(socket.output()).isEqualTo(expected);
-    }
 
     @Test
     void index() throws IOException, URISyntaxException {
@@ -41,7 +30,7 @@ class RequestHandlerTest {
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final RequestHandler handler = new RequestHandler(socket);
+        final RequestHandler handler = new RequestHandler(socket, initUrlHandlerMapping(), initHandlerMapping());
 
         // when
         handler.run();
@@ -49,12 +38,25 @@ class RequestHandlerTest {
         // then
 
 
-        var expected = "HTTP/1.1 200 \r\n" +
+        var expected = "HTTP/1.1 200 OK \r\n" +
                 "Content-Type: text/html;charset=utf-8 \r\n" +
                 "Content-Length: 6902 \r\n" +
                 "\r\n" +
                 new String(FileIoUtils.loadFileFromClasspath("templates/index.html"));
 
         assertThat(socket.output()).isEqualTo(expected);
+    }
+
+    private static List<Handler> initHandlerMapping() {
+        List<Handler> handlerMapping = new ArrayList<>();
+        handlerMapping.add(new HtmlRequestHandler());
+        handlerMapping.add(new StaticResourceRequestHandler());
+        return handlerMapping;
+    }
+
+    private static Map<String, Handler> initUrlHandlerMapping() {
+        HashMap<String, Handler> urlHandlerMapping = new HashMap<>();
+        urlHandlerMapping.put("/user/create", new UserApiHandler());
+        return urlHandlerMapping;
     }
 }
